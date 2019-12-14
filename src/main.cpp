f62407ca3f68a3294992de4b9ece59cf26587cf4
TheNumbat <mslater@nevada.unr.edu>
2019-12-14 09:33:01 -0500

    base

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_sdl.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <misc/flags.h>
+
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+
+SDL_Window* window = nullptr;
+SDL_GLContext gl_context = nullptr;
+
+void plt_setup() {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	window = SDL_CreateWindow("s4d", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+	gl_context = SDL_GL_CreateContext(window);
+	SDL_GL_MakeCurrent(window, gl_context);
+	SDL_GL_SetSwapInterval(1);
+
+	gladLoadGL();
+
+	ImGui::CreateContext();
+	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+	ImGui_ImplOpenGL3_Init();
+
+	ImGui::StyleColorsDark();
+}
+
+void plt_shutdown() {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_GL_DeleteContext(gl_context);
+	SDL_DestroyWindow(window);
+	window = nullptr;
+	gl_context = nullptr;
+	SDL_Quit();	
+}
+
+void render_frame() {
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+	SDL_GL_SwapWindow(window);
+}
+
+void begin_frame() {
+
+	glClearColor(0.6f, 0.65f, 0.7f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame(window);
+	ImGui::NewFrame();
+}
+
+int main(int argc, char** argv) {
+
+	plt_setup();
+
+	ImGui::GetStyle().WindowRounding = 0.0f;
+
+	bool running = true;
+	while(running) {
+	
+		begin_frame();
+		ImGuiIO& io = ImGui::GetIO();
+
+		SDL_Event e;
+		while(SDL_PollEvent(&e)) {
+
+			ImGui_ImplSDL2_ProcessEvent(&e);
+
+			switch(e.type) {
+			case SDL_QUIT: {
+				running = false;
+			} break;
+			case SDL_KEYDOWN: {
+				if(e.key.keysym.sym == SDLK_ESCAPE) {
+					running = false;
+				}
+			} break;
+			}
+		}
+
+		render_frame();
+	}
+
+	plt_shutdown();
+	return 0;
+}
